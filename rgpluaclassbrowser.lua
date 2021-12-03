b160456bb3de18bac1c7a5e1eb569114a15a158b
Robert Patterson <robert@robertgpatterson.com>
2021-12-03 11:12:22 -0600

    use timer to parse xml after dialog is visible, to show that something is happening

--- rgpluaclassbrowser.lua
+++ rgpluaclassbrowser.lua
@@ -20,6 +20,8 @@ global_class_index = nil
 global_dialog = nil
 global_dialog_info = {}     -- key: list control id or hard-coded string, value: table of associated data and controls
 global_control_xref = {}    -- key: non-list control id, value: associated list control id
+global_timer_id = 1         -- per docs, we supply the timer id, starting at 1
+global_progress_label = nil
 
 current_class_name = ""
 changing_class_name_in_progress = false
@@ -35,9 +37,16 @@ end
 
 function get_edit_text(edit_control)
     if not edit_control then return "" end
-    local str = finale.FCString()
-    edit_control:GetText(str)
-    return str.LuaString
+    local fcstring = finale.FCString()
+    edit_control:GetText(fcstring)
+    return fcstring.LuaString
+end
+
+function set_text(control, text)
+    if not control then return end
+    local fcstring = finale.FCString()
+    fcstring.LuaString = text
+    control:SetText(fcstring)
 end
     
 function method_info(class_info, method_name)
@@ -200,21 +209,16 @@ function on_method_selection(list_control, index)
                 local is_property = nil == method_info.arglist
                 local dot = ":"
                 if is_property then dot = "." end
-                local fcstring = finale.FCString()
-                fcstring.LuaString = method_info.class .. dot .. method_name
-                list_info.fullname_static:SetText(fcstring)
+                set_text(list_info.fullname_static, method_info.class .. dot .. method_name)
                 if is_property then
                     local methods_list_info = global_dialog_info[global_control_xref["methods"]]
                     local property_getter_info = methods_list_info.current_strings["Get" .. method_name]
                     if property_getter_info then
-                        fcstring.LuaString = property_getter_info.returns
-                        list_info.returns_static:SetText(fcstring)
+                        set_text(list_info.returns_static, property_getter_info.returns)
                     end
                 else
-                    fcstring.LuaString = method_info.returns
-                    list_info.returns_static:SetText(fcstring)
-                    fcstring.LuaString = method_info.arglist
-                    list_info.arglist_static:SetText(fcstring)
+                    set_text(list_info.returns_static, method_info.returns)
+                    set_text(list_info.arglist_static, method_info.arglist)
                 end
                 show = true
             end
@@ -288,6 +292,67 @@ function on_doc_button(button_control)
     end
 end
 
+get_eligible_classes = function()
+    set_text(global_progress_label, "Getting eligible classes from Lua state...")
+    local retval = {}
+    for k, v in pairs(_G.finale) do
+        local kstr = tostring(k)
+        if kstr:find("FC") == 1  then
+            retval[kstr] = 1
+        end    
+    end
+    return retval
+end
+
+create_class_index = function()
+    local xml2lua = require("xml2lua")
+    local handler = require("xmlhandler.tree")
+
+    set_text(global_progress_label, "Parsing xml for class and method info...")
+
+    local jwhandler = handler:new()
+    local jwparser = xml2lua.parser(jwhandler)
+    -- parsing the xml croaks the debugger because of the size of the xml--don't try to debug it
+    jwparser:parse(xml2lua.loadFile(finenv.RunningLuaFolderPath() .. "/jwluatagfile.xml"))
+
+    local jwlua_compounds = jwhandler.root.tagfile.compound
+    local temp_class_index = {}
+    for i1, t1 in pairs(jwlua_compounds) do
+        if t1._attr and t1._attr.kind == "class" then
+            set_text(global_progress_label, "Indexing " .. t1.name .. "...")
+            temp_class_index[t1.name] = t1
+            local members_index = {}
+            if t1.member then
+                if #t1.member <= 1 then
+                    if t1.member._attr and t1.member._attr.kind == "function" then
+                        members_index[t1.member.name] = t1.member
+                    end
+                elseif #t1.member > 1 then
+                    for i2, t2 in pairs(t1.member) do
+                        if t2._attr and t2._attr.kind == "function" then
+                            members_index[t2.name] = t2
+                        end
+                    end
+                end
+            end
+            temp_class_index[t1.name].__members = members_index
+        end
+    end
+    return temp_class_index
+end
+
+function on_timer(timer_id)
+    if timer_id ~= global_timer_id then return end
+    global_timer_id = 0 -- blocks further calls to this function
+    global_dialog:StopTimer(timer_id)
+    eligible_classes = get_eligible_classes()
+    global_class_index = create_class_index()
+    --require('mobdebug').start() -- uncomment this to debug (after creation of global_class_index because it takes forever in debugger to parse the xml)
+    update_classlist()
+    set_text(global_progress_label, "")
+    global_progress_label:SetVisible(false)
+end
+
 local create_dialog = function()
     local y = 0
     local vert_sep = 25
@@ -308,10 +373,8 @@ local create_dialog = function()
     
     local create_static = function(dialog, text, this_col_width)
         local static_text = dialog:CreateStatic(x, y)
-        local fcstring = finale.FCString()
-        fcstring.LuaString = text
         static_text:SetWidth(this_col_width)
-        static_text:SetText(fcstring)
+        set_text(static_text, text)
         return static_text
     end
     
@@ -359,7 +422,6 @@ local create_dialog = function()
     
     local create_display_area = function(dialog, list_info, width, is_for_properties)
         is_for_properties = is_for_properties or false
-        local fcstring = finale.FCString()
         list_info.fullname_static = dialog:CreateStatic(x, y)
         list_info.fullname_static:SetWidth(width)
         list_info.fullname_static:SetVisible(false)
@@ -368,9 +430,9 @@ local create_dialog = function()
         local my_x = x
         list_info.returns_label = dialog:CreateStatic(my_x, y)
         if is_for_properties then
-            fcstring.LuaString = "Type:"
+            set_text(list_info.returns_label, "Type:")
         else
-            fcstring.LuaString = "Returns:"
+            set_text(list_info.returns_label, "Returns:")
         end
         local label_width = 35
         if not is_for_properties then
@@ -379,7 +441,6 @@ local create_dialog = function()
         local doc_button_width = 40
         local my_x_sep = 1
         local return_static_width = width - label_width - doc_button_width - (2*my_x_sep)
-        list_info.returns_label:SetText(fcstring)
         list_info.returns_label:SetWidth(label_width)
         list_info.returns_label:SetVisible(false)
         my_x = my_x + label_width + my_x_sep
@@ -391,16 +452,14 @@ local create_dialog = function()
         list_info.method_doc_button:SetWidth(doc_button_width)
         list_info.method_doc_button:SetVisible(false)
         global_control_xref[list_info.method_doc_button:GetControlID()] = list_info.list_box:GetControlID()
-        fcstring.LuaString = "Doc."
-        list_info.method_doc_button:SetText(fcstring)
+        set_text(list_info.method_doc_button, "Doc.")
         list_info.method_doc_button:SetVisible(false)
         dialog:RegisterHandleControlEvent(list_info.method_doc_button, on_doc_button)
         if not is_for_properties then
             y = y + my_vert_sep
             my_x = x
             list_info.arglist_label = dialog:CreateStatic(my_x, y)
-            fcstring.LuaString = "Params:"
-            list_info.arglist_label:SetText(fcstring)
+            set_text(list_info.arglist_label, "Params:")
             list_info.arglist_label:SetWidth(label_width)
             list_info.arglist_label:SetVisible(false)
             my_x = my_x + label_width + my_x_sep
@@ -420,14 +479,25 @@ local create_dialog = function()
         end
     end
 
-    -- scratch FCString
-    local str = finale.FCString()
     -- create a new dialog
     local dialog = finale.FCCustomLuaWindow()
+    local str = finale.FCString() -- scratch
     str.LuaString = "RGP Lua - Class Browser"
     dialog:SetTitle(str)
-    dialog:RegisterInitWindow(update_classlist)
     dialog:RegisterHandleCommand(on_list_select)
+    --[[
+    -- normally we would just use RegisterInitWindow to populate a modeless dialog,
+    -- but this dialog takes a long time to process jwluatagfile.xml so we do it
+    -- it in a timer handler allowing us to show progress. The UI is blocked while it happens,
+    -- mainly because the xml has to be processed in one go.
+    dialog:RegisterInitWindow(update_classlist)
+    ]]
+    dialog:RegisterInitWindow(
+        function()
+            global_dialog:SetTimer(global_timer_id, 1) -- timer can't be set until window is created
+        end
+    )
+    dialog:RegisterHandleTimer(on_timer)
     
     local classes_list = create_column(dialog, 400, col_width, "Classes:", on_class_selection,
         function(control)
@@ -435,8 +505,7 @@ local create_dialog = function()
         end)
     global_control_xref["classes"] = classes_list:GetControlID()
     local class_doc = dialog:CreateButton(x, y)
-    str.LuaString = "Class Documentation"
-    class_doc:SetText(str)
+    set_text(class_doc, "Class Documentation")
     class_doc:SetWidth(col_width)
     dialog:RegisterHandleControlEvent(class_doc,
         function(control)
@@ -447,6 +516,11 @@ local create_dialog = function()
         end
     )
     x = x + col_width + sep_width
+    global_progress_label = dialog:CreateStatic(x, y)
+    global_progress_label:SetWidth(2.5*col_width)
+    --macOS does not update label text in real time, but Windows does.
+    --Therefore on macOS this is the only text that shows in the label
+    set_text(global_progress_label, "initializing...")
     
     local properties_list = create_column(dialog, 170, col_width + col_extra, "Properties:", on_method_selection, handle_edit_control)
     global_control_xref["properties"] = properties_list:GetControlID()
@@ -464,54 +538,8 @@ local create_dialog = function()
     
     -- create close button
     local ok_button = dialog:CreateOkButton()
-    str.LuaString = "Close"
-    ok_button:SetText(str)
+    set_text(ok_button, "Close")
     return dialog
-end
-
-local get_eligible_classes = function()
-    local retval = {}
-    for k, v in pairs(_G.finale) do
-        local kstr = tostring(k)
-        if kstr:find("FC") == 1  then
-            retval[kstr] = 1
-        end    
-    end
-    return retval
-end
-
-local create_class_index = function()
-    local xml2lua = require("xml2lua")
-    local handler = require("xmlhandler.tree")
-
-    local jwhandler = handler:new()
-    local jwparser = xml2lua.parser(jwhandler)
-    -- parsing the xml croaks the debugger because of the size of the xml--don't try to debug it
-    jwparser:parse(xml2lua.loadFile(finenv.RunningLuaFolderPath() .. "/jwluatagfile.xml"))
-
-    local jwlua_compounds = jwhandler.root.tagfile.compound
-    local temp_class_index = {}
-    for i1, t1 in pairs(jwlua_compounds) do
-        if t1._attr and t1._attr.kind == "class" then
-            temp_class_index[t1.name] = t1
-            local members_index = {}
-            if t1.member then
-                if #t1.member <= 1 then
-                    if t1.member._attr and t1.member._attr.kind == "function" then
-                        members_index[t1.member.name] = t1.member
-                    end
-                elseif #t1.member > 1 then
-                    for i2, t2 in pairs(t1.member) do
-                        if t2._attr and t2._attr.kind == "function" then
-                            members_index[t2.name] = t2
-                        end
-                    end
-                end
-            end
-            temp_class_index[t1.name].__members = members_index
-        end
-    end
-    return temp_class_index
 end
 
 local open_dialog = function()
@@ -520,11 +548,4 @@ local open_dialog = function()
     global_dialog:ShowModeless()
 end
 
---ToDo: maybe do a modal here to show progress?
-
-global_class_index = create_class_index()
-
---require('mobdebug').start() -- uncomment this to debug (after creation of global_class_index because it takes forever in debugger to parse the xml)
-
-eligible_classes = get_eligible_classes()
 open_dialog()
