81bdebda3927ecbcaa8ec9ffcd5bfaaca287a2cb
Robert Patterson <robert@robertgpatterson.com>
2024-01-02 13:13:44 -0600

    remove print statement

--- rgpluaconsole.lua
+++ rgpluaconsole.lua
@@ -556,7 +556,6 @@ local function write_line_numbers(num_lines)
             if unichar ~= string.byte("\n") and unichar ~= string.byte("\r") then
                 do_number = false
             end
-            print(line_number, unichar, do_number)
         end
         local line_ending = i < num_lines and "\n" or ""
         if do_number then
